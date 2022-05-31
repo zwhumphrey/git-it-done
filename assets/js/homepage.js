2a9d5805eba72f7fcd5d0633eabc684e199ca277
zwhumphrey <103855578+zwhumphrey@users.noreply.github.com>
2022-05-31 16:05:02 -0500

    added js functions to search user names on github

---
+++ assets/js/homepage.js
@@ -0,0 +1,30 @@
+var getUserRepos = function(user) {
+    // format the github api url
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
+  
+    // make a request to the url
+    fetch(apiUrl).then(function(response) {
+      response.json().then(function(data) {
+        console.log(data);
+      });
+    });
+  };
+
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+
+var formSubmitHandler = function(event) {
+  event.preventDefault();
+  // get value from input element
+var username = nameInputEl.value.trim();
+
+if (username) {
+  getUserRepos(username);
+  nameInputEl.value = "";
+} else {
+  alert("Please enter a GitHub username");
+}
+  console.log(event);
+};
+
+userFormEl.addEventListener("submit", formSubmitHandler);
