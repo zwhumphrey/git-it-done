19552968a344bcbca7ae70a5a1726847cebfe123
zwhumphrey <103855578+zwhumphrey@users.noreply.github.com>
2022-05-31 19:12:06 -0500

    add Js for repo page added the for loop to have it run a function for ech repo)

---
+++ assets/js/single.js
@@ -0,0 +1,29 @@
+var getRepoIssues = function(repo) {
+    console.log(repo);
+    var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
+
+    fetch(apiUrl).then(function(response) {
+        // request was successful
+        if (response.ok) {
+          response.json().then(function(data) {
+            // pass response data to dom function
+            displayIssues(data);
+          });
+        }
+        else {
+          alert("There was a problem with your request!");
+        }
+      });
+  };
+  
+  getRepoIssues("facebook/react");
+
+  var displayIssues = function(issues) {
+    for (var i = 0; i < issues.length; i++) {
+        // create a link element to take users to the issue on github
+        var issueEl = document.createElement("a");
+        issueEl.classList = "list-item flex-row justify-space-between align-center";
+        issueEl.setAttribute("href", issues[i].html_url);
+        issueEl.setAttribute("target", "_blank");
+      }
+};
